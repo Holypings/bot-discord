2a372a7748411c85ee78c692773cdc65f12cd8e7
Holypings <olivia.icard03@gmail.com>
2025-10-07 20:54:16 +0200

    Préparer le bot avec token caché

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-aa
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+# Charger le fichier .env
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} est connecté !")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("Pong !")
+
+bot.run(TOKEN)
